c073208fa9d7a6ee5aab2338bfaa43fd69a0b538
mfauzirh <mfauzirizkih@gmail.com>
2024-11-17 17:49:18 +0700

    get to know with comparation operation

---
+++ 01-belajar-golang-dasar/11-comparation-operation.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+/*
+ * Comparator operator is operation to compare two values,
+ * It will result boolean value (true / false)
+ * >, <, >=, <=, ==, !=
+ */
+
+func main() {
+	fmt.Println("Fauzi" == "Fauzi")
+	fmt.Println(20 > 10)
+}

5cf2b66b326f62516499c48ae1bb5773cad65f45
mfauzirh <mfauzirizkih@gmail.com>
2024-11-19 07:17:52 +0700

    get to know with math package

---
+++ 02-belajar-golang-standard-library/07-math.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(1.40))
+	fmt.Println(math.Floor(1.40))
+	fmt.Println(math.Round(1.40))
+	fmt.Println(math.Max(100, 1000))
+	fmt.Println(math.Max(100, 1000))
+	fmt.Println(math.Abs(100 - 1000))
+}

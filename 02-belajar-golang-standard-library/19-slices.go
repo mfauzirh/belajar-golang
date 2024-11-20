ae37fcc21627304cd6221e90608e20bd37b75e7d
mfauzirh <mfauzirizkih@gmail.com>
2024-11-20 07:39:12 +0700

    get to know with slices package

---
+++ 02-belajar-golang-standard-library/19-slices.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	names := []string{"John", "Paul", "George", "Ringo"}
+	values := []int{10, 1, 3}
+
+	fmt.Println(slices.Min(names))
+	fmt.Println(slices.Min(values))
+	fmt.Println(slices.Max(names))
+	fmt.Println(slices.Max(values))
+
+	fmt.Println(slices.Contains(names, "Eko"))
+}

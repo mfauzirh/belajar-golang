6b51eff8426e5a5af87cdeb7a5593ff059f65a5c
mfauzirh <mfauzirizkih@gmail.com>
2024-11-20 09:00:46 +0700

    get to know with io

---
+++ 02-belajar-golang-standard-library/20-path.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"path"
+	"path/filepath"
+)
+
+func main() {
+	fmt.Println(path.Dir("hello/world.go "))
+	fmt.Println(path.Dir("hello/world.go"))
+	fmt.Println(path.Ext("hello/world.go"))
+	fmt.Println(path.Join("hello", "world", "main.go"))
+
+	fmt.Println(filepath.Dir("hello/world.go "))
+	fmt.Println(filepath.Dir("hello/world.go"))
+	fmt.Println(filepath.Ext("hello/world.go"))
+	fmt.Println(filepath.Join("hello", "world", "main.go"))
+}
